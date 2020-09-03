b204c594330ceec9db46355300fd90b0964a67bd
MCambie <cambie.marie@gmail.com>
2020-09-03 09:38:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# m_bois"  git init git add README.md git commit -m "first commit" git branch -M master git remote add origin https://github.com/MCambie/m_bois.git git push -u origin master
---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+
+// Bouton back-to-top
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+//   Style au scroll
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".main-nav");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+  // Gallery
+
+  $(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('animation');
+    $("#gallery").fadeTo(300, 1);
+    }, 300);
+    });
+    });
