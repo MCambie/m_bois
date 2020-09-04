e005b131ee63cdcdf1077602c909f238c821dc34
Marie Cambie <cambie.marie@gmail.com>
2020-09-04 11:20:34 +0200

    slashscreen ok carroussel scroll responsive

--- src/js/main.js
+++ src/js/main.js
@@ -22,14 +22,23 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
-//   Style au scroll
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".main-nav");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
+//   Style du menu au scroll
+$('body').css('padding-top', $('.main-nav').outerHeight() + 'px')
 
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
   // Gallery
 
   $(function() {
